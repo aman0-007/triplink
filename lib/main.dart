37192c79a32141519b1b80252ad5f86bba706aa3
aman0-007 <dwivedi.aman2811@gmail.com>
2025-01-14 10:52:10 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:triplink/themes.dart';
 
-import 'screens/login_screen/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: TripLinkTheme.lightTheme,
       darkTheme: TripLinkTheme.darkTheme,
       themeMode: ThemeMode.system,
-      home: const LoginScreen(),
+      home: const MyHomePage(),
     );
   }
 }
---
+++ lib/screens/userscreens/homepage.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+//
+// class HomePage extends StatefulWidget {
+//
+// }
